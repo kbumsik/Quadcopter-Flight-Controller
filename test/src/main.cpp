18805eae99d3ca171b750fca6cec74e058c25ff2
BUM SIK KIM <bumsik.kim@stonybrook.edu>
2015-07-08 16:09:19 -0400

    create C++ project again

---
+++ test/src/main.cpp
@@ -0,0 +1,24 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#define STM32F411xE
+#define USE_HAL_DRIVER
+
+#include <stdint.h>
+#include "stm32f4xx.h"
+#include "stm32f4xx_nucleo.h"
+			
+
+int main(void)
+{
+
+
+}
